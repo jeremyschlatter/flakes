c0c41a292dd14e5b83009752849e9092fbb8068a
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-23 12:13:46 -0700

    update flake: llm-viz

--- llm-viz/flake.nix
+++ llm-viz/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    nixpkgs-old.url = github:NixOS/nixpkgs/release-22.05;
+    nixpkgs-old.url = github:NixOS/nixpkgs/release-24.05;
     flake-utils.url = github:numtide/flake-utils;
   };
 
