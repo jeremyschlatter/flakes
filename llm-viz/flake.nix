cd23b810b314c816e3a55eb9e2d8ada20edb108e
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-23 15:42:57 -0700

    llm-viz: clean up flake

--- llm-viz/flake.nix
+++ llm-viz/flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
     nixpkgs-old.url = github:NixOS/nixpkgs/38e5815;
-    # nixpkgs-old.url = github:NixOS/nixpkgs/release-24.05;
     flake-utils.url = github:numtide/flake-utils;
   };
 
@@ -15,24 +14,7 @@
     in {
       devShell = mkShellNoCC {
         packages = lib.attrsets.mapAttrsToList writeShellScriptBin scripts ++ [
-          # old.x86_64-darwin.odin
           old.${system}.odin
-
-#           ((odin.override {
-#             llvmPackages = llvmPackages_17;
-#           }).overrideAttrs (attrs: {
-#             version = "dev-2023-08";
-#             src = attrs.src.override {
-#               hash = "sha256-pmgrauhB5/JWBkwrAm7tCml9IYQhXyGXsNVDKTntA0M=";
-#             };
-#             patches = [];
-#             postPatch = ''
-#               substituteInPlace src/build_settings.cpp \
-#                 --replace-fail "arm64-apple-macosx" "arm64-apple-darwin"
-#               patchShebangs --build build_odin.sh
-#             '';
-#           }))
-
           yarn
         ];
       };
