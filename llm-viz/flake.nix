bd4a36f15356a64511d71ce1da66a595b37aba29
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-23 20:54:25 -0700

    llm-viz: shorter flake

--- llm-viz/flake.nix
+++ llm-viz/flake.nix
@@ -1,23 +1,15 @@
 {
-  inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    nixpkgs-old.url = github:NixOS/nixpkgs/38e5815;
-    flake-utils.url = github:numtide/flake-utils;
-  };
+  inputs.nixpkgs-old.url = github:NixOS/nixpkgs/38e5815;
 
   outputs = { self, nixpkgs, nixpkgs-old, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
     with nixpkgs.legacyPackages.${system};
-    let
-      scripts = {};
-      old = nixpkgs-old.legacyPackages;
-    in {
+    {
       devShell = mkShellNoCC {
-        packages = lib.attrsets.mapAttrsToList writeShellScriptBin scripts ++ [
-          old.${system}.odin
+        packages = [
+          nixpkgs-old.legacyPackages.${system}.odin
           yarn
         ];
       };
     });
 }
-
