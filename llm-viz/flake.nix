a16f07b5309444dd361360a752ecd15cc1c0e0e5
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-23 22:08:04 -0700

    update flake: llm-viz

--- llm-viz/flake.nix
+++ llm-viz/flake.nix
@@ -1,12 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/38e5815;
+    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs-old.url = github:NixOS/nixpkgs/38e5815;
     mkShell.url = github:jeremyschlatter/mkShell;
   };
 
-  outputs = { self, nixpkgs, mkShell }:
-    mkShell nixpkgs (pkgs: with pkgs; [
-      odin
+  outputs = { self, nixpkgs, nixpkgs-old, mkShell }:
+    mkShell.withSystem nixpkgs (pkgs: system: with pkgs; [
+      nixpkgs-old.legacyPackages.${system}.odin
       yarn
     ]);
 }
