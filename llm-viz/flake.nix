8745d024cae22d2189a113f190e068e430cb9e52
Jeremy Schlatter <jeremy@jeremyschlatter.com>
2025-10-28 17:21:34 -0700

    llm-viz: add more comments

--- llm-viz/flake.nix
+++ llm-viz/flake.nix
@@ -1,6 +1,11 @@
 {
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    # Selected this commit to grab odin dev-2023-05.
+    # There's only a relatively narrow range of odin versions that
+    # work with llm-viz. Old odin versions do not accept all of the
+    # command line arguments that llm-viz uses. New versions do not
+    # accept all of the syntax that llm-viz uses.
     nixpkgs-old.url = github:NixOS/nixpkgs/38e5815;
     mkShell.url = github:jeremyschlatter/mkShell;
   };
@@ -8,6 +13,7 @@
   outputs = { self, nixpkgs, nixpkgs-old, mkShell }:
     mkShell nixpkgs (pkgs: system: with pkgs; [
       (nixpkgs-old.legacyPackages.${system}.odin.overrideAttrs (attrs: {
+        # This is unnecessary, but it makes `odin version` print the right thing.
         # build_odin.sh used the date of the build as the version stamp.
         # That's misleading when we're building a version that's 2+ years old.
         # Use the tagged version name instead.
